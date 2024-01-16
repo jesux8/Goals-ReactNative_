bfd56770522257ac13ce73c2203cf4e80f600fb8
jesux8 <jesusalejowk@gmail.com>
2024-01-16 09:22:53 +0100

    alpha1

--- App.js
+++ App.js
@@ -1,14 +1,46 @@
 import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <TextInput placeholeder='Input your Goal'/>
-      <Button title='Add Goal'/>
+      <View style={styles.inputContainer}>
+        <TextInput
+        style={styles.textInput}
+        placeholeder='Input your Goal' />
+        <Button title='Add Goal' />
+      </View>
+
+      <View style={styles.container}>
+        <Text>
+          Your list of goals
+        </Text>
+      </View>
     </View>
-    <View>
-      <Text>Your list of goals</Text>
-    </View>
-     
+  
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 70,
+    paddingHorizontal: 25
+
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingBottom: 20,
+    alignItems: 'center',
+    borderBottomWidth: 1,
+  },
+  textInput: {
+    paddingStart: 10,
+    borderWidth: 1,
+    width: "70%",
+  },
+  goalsContainer: {
+    flex: 4,
+  }
+});
